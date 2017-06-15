bd9b26699fd1e27dbf063f86cc7fb726e553685a
lihaihao <mashy1024@gmail.com>
2017-06-16 07:13:52 +0800

    init

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ get "/jquery-1" => "pages#jquery_1"
 get "/jquery-2" => "pages#jquery_2"
 get "/jquery-3" => "pages#jquery_3"
 get "/jquery-4" => "pages#jquery_4"
-get "/jquert-5" => "pages#jquery_5"
+get "/jquery-5" => "pages#jquery_5"
 
 root "pages#jquert_1"
 end
